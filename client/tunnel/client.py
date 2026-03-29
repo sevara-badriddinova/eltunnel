e7de062d4ce4d2e90e043b4f32d415878c21c62d
sevbad <sbadriddinova@usf.edu>
2026-03-28 21:19:02 -0400

    proxy routing, pending requests map, channel setup

--- client/tunnel/client.py
+++ client/tunnel/client.py
@@ -1,13 +1,15 @@
 import asyncio
 import json
 import websockets
+import string
+import random
 
 async def connect(server_url, port):
     message = {
         "type": "register",
         "request_id": "",
         "payload": {
-            "subdomain": "alice"
+            "subdomain": random_subdomain()
         }
     }
     message_json = json.dumps(message)
@@ -15,7 +17,11 @@ async def connect(server_url, port):
         await ws.send(message_json)
         response = await ws.recv()
         response = json.loads(response)
-        print(response)
+        print("Tunnel ready at response " + response["Payload"]["URL"])
 
+def random_subdomain():
+    res = random.choices(string.ascii_lowercase, k = 5)
+    return ''.join(res)
+    
 if __name__ == "__main__":
     asyncio.run(connect("ws://localhost:8081/tunnel", 3000))
